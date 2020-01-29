4bb9320fa627ae2df7f601aad7a772eda64ea14f
katebrune <kbrune@iastate.edu>
2020-01-29 11:43:32 -0600

    added basic menu to dashboard page

--- samaritan/src/components/dashboard/dashboard.tsx
+++ samaritan/src/components/dashboard/dashboard.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Layout, Menu, Icon } from "antd";
 
 const { Content, Sider } = Layout;
-const { SubMenu } = Menu;
 
 class Dashboard extends React.Component {
   state = {
@@ -25,41 +24,17 @@ class Dashboard extends React.Component {
             <div className="logo" />
             <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
               <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>Option 1</span>
+                <Icon type="appstore" />
+                <span>Dashboard</span>
               </Menu.Item>
               <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
+                <Icon type="search" />
+                <span>Search</span>
               </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    <span>Option 3</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="3">1</Menu.Item>
-                <Menu.Item key="4">2</Menu.Item>
-                <Menu.Item key="5">3</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                    <Icon type="team" />
-                    <span>Option 4</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="6">1</Menu.Item>
-                <Menu.Item key="8">2</Menu.Item>
-              </SubMenu>
+
               <Menu.Item key="9">
-                <Icon type="file" />
-                <span>Option 5</span>
+                <Icon type="setting" />
+                <span>Settings</span>
               </Menu.Item>
             </Menu>
           </Sider>
