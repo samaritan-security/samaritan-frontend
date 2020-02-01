81b1bdc2fc12a928991103708caa842a4000d3f3
stoosi <stoosi@iastate.edu>
2020-02-01 15:24:50 -0600

    basic components done

--- samaritan/src/components/login/login.tsx
+++ samaritan/src/components/login/login.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Input } from 'antd';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import './login.css'
+
 
 class Login extends React.Component {
   render() {
     return (
       <>
-        <p>Login</p>
+        <div className="login" >
+          <Input placeholder="Username" style={{marginBottom: 10, }}/>
+          <Input.Password placeholder="Password" style={{marginBottom: 10, }}/>
+          <Button type="primary" block>
+            Login
+          </Button>
+        </div>
       </>
     );
   }
