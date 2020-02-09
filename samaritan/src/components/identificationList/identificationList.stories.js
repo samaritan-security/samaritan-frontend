195f89359f3a92eda4e989e73c46abbff7c0f9d3
katebrune <kbrune@iastate.edu>
2020-02-09 15:30:16 -0600

    added folder/files for identification list

---
+++ samaritan/src/components/identificationList/identificationList.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+import IdentificationList from "./identificationList";
+import "antd/dist/antd.css";
+
+export default { title: "IdentificationList" };
+
+export const login = () => <IdentificationList />;
---
+++ samaritan/src/components/identificationList/identificationList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Avatar } from "antd";
+
+class IdentificationList extends React.Component {
+  render() {
+    return (
+      <>
+        <Avatar icon="user" />
+        <p>IdentificationList</p>
+      </>
+    );
+  }
+}
+
+export default IdentificationList;
