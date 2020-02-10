bfd7de83282a10176ce5bf5207f38b3140465422
katebrune <kbrune@iastate.edu>
2020-02-10 16:14:29 -0600

    fixed error with unknown handler path going to known endpoint

--- samaritan/src/api/unknown-handler.ts
+++ samaritan/src/api/unknown-handler.ts
@@ -7,7 +7,7 @@ export class UnknownHandler {
    */
   async getAllUnknown(): Promise<Unknown[]> {
     let unknown: Unknown[] = [];
-    let data = await APIHandler(`known`, "GET");
+    let data = await APIHandler(`unknown`, "GET");
     if (!!data) {
       let dataArray = JSON.parse(JSON.stringify(data));
       for (let i = 0; i < dataArray.length; i++) {
