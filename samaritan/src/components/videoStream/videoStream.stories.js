d2c7b8eca5ff1dc336f81b307116a0d3c734485f
katebrune <kbrune@iastate.edu>
2020-02-10 12:03:13 -0600

    updated story ip

--- samaritan/src/components/videoStream/videoStream.stories.js
+++ samaritan/src/components/videoStream/videoStream.stories.js
@@ -3,4 +3,4 @@ import VideoStream from "./videoStream";
 
 export default { title: "VideoStream" };
 
-export const videoStream = () => <VideoStream ip="192.168.1.110" />;
+export const videoStream = () => <VideoStream ip="192.168.1.107" />;
