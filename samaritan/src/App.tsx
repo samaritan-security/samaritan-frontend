6f2479f43916dfe6a0cf64d10d91ab7ed138e031
katebrune <kbrune@iastate.edu>
2020-02-11 01:15:49 -0600

    fixed dependencies, got rid of unused dependencies

--- samaritan/src/App.tsx
+++ samaritan/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "antd/dist/antd.css";
-import VideoStream from "./components/videoStream/videoStream";
 
 const App: React.FC = () => {
   return (
