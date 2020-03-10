508a6375e83669e21b5ec22bfcc511b7a7c437f7
Kate Brune <kbrune@iastate.edu>
2020-03-09 23:26:23 -0500

    added alert details popup when u click on an alert

--- src/api/person-handler.ts
+++ src/api/person-handler.ts
@@ -1,40 +1,47 @@
-import { Person } from './api-types'
-import { APIHandler } from './api-handler'
+import { Person } from "./api-types";
+import { APIHandler } from "./api-handler";
 
 export class PersonHandler {
-    /**
-     * returns all known people
-     */
-    async getAllKnownPeople(): Promise<Person[]>{
-        let people: Person[] = [];
-        let data = await APIHandler(`people/known`, "GET");
-        if (!!data) {
-            let dataArray = JSON.parse(JSON.stringify(data));
-            for (let i = 0; i < dataArray.length; i++) {
-            people.push(new Person(dataArray[i]));
-            }
-        }
-        return people;
+  /**
+   * returns all known people
+   */
+  async getAllKnownPeople(): Promise<Person[]> {
+    let people: Person[] = [];
+    let data = await APIHandler(`people/known`, "GET");
+    if (!!data) {
+      let dataArray = JSON.parse(JSON.stringify(data));
+      for (let i = 0; i < dataArray.length; i++) {
+        people.push(new Person(dataArray[i]));
+      }
     }
+    return people;
+  }
 
-    /**
-     * returns all unknown people
-     */
-    async getAllUnknownPeople(): Promise<Person[]>{
-        let people: Person[] = [];
-        let data = await APIHandler(`people/unknown`, "GET");
-        if (!!data) {
-            let dataArray = JSON.parse(JSON.stringify(data));
-            for (let i = 0; i < dataArray.length; i++) {
-            people.push(new Person(dataArray[i]));
-            }
-        }
-        return people;
+  /**
+   * returns all unknown people
+   */
+  async getAllUnknownPeople(): Promise<Person[]> {
+    let people: Person[] = [];
+    let data = await APIHandler(`people/unknown`, "GET");
+    if (!!data) {
+      let dataArray = JSON.parse(JSON.stringify(data));
+      for (let i = 0; i < dataArray.length; i++) {
+        people.push(new Person(dataArray[i]));
+      }
     }
+    return people;
+  }
 
-    async getById(id : string) : Promise<Person>{
-        let data = await APIHandler(`people/${id}`, "GET");
-        let person : Person = new Person(data);
-        return person;
-    }
+  async getById(id: string): Promise<Person> {
+    let data = await APIHandler(`people/${id}`, "GET");
+    let person: Person = new Person(data);
+    return person;
+  }
+}
+
+export class MockPersonHandler {
+  getById(id: string): Person {
+    let data = new Person({ _id: 0, name: "Foo Bar", img: "" });
+    return data;
+  }
 }
--- src/components/alertPage/alertPage.tsx
+++ src/components/alertPage/alertPage.tsx
@@ -1,80 +1,137 @@
-import React from 'react'
-import {Typography, Table} from 'antd'
-import {Alert} from '../../api/api-types'
-import { AlertHandler, MockAlertHandler } from '../../api/alert-handler'
-import { AlertState } from 'antd/lib/alert'
+import React from "react";
+import { Typography, Table, Modal, Avatar } from "antd";
+import { Alert, Person } from "../../api/api-types";
+import { AlertHandler, MockAlertHandler } from "../../api/alert-handler";
+import { AlertState } from "antd/lib/alert";
+import ActionRequest from "../../custom-components/actionRequest/actionRequest";
+import { PersonHandler, MockPersonHandler } from "../../api/person-handler";
 
-const {Title} = Typography
+const { Title } = Typography;
 
-interface IAlertPageProps {
-
-}
+interface IAlertPageProps {}
 interface IAlertPageState {
-  alerts : Alert[],
-  startTime : string
+  alerts: Alert[];
+  startTime: string;
+  visible: boolean;
+  person: Person;
 }
 
 class AlertPage extends React.Component<IAlertPageProps, IAlertPageState> {
   state = {
-    alerts : [],
-    startTime : ""
-  }
+    alerts: [],
+    startTime: "",
+    visible: false,
+    person: new Person({})
+  };
 
   componentDidMount = () => {
+    this.setState({
+      startTime: new Date().toUTCString()
+    });
+    setInterval(() => {
+      const { startTime } = this.state;
+      let endTime = new Date().toUTCString();
+      // new AlertHandler().getAlerts(startTime, endTime).then(data => {
+      //   this.setState({
+      //     alerts : data
+      //   })
+      // })
+      let response = new MockAlertHandler().getAlerts(startTime, endTime);
       this.setState({
-        startTime : new Date().toUTCString()
-      })
-      setInterval(() => {
-        const {startTime} = this.state;
-        let endTime = new Date().toUTCString()
-        // new AlertHandler().getAlerts(startTime, endTime).then(data => {
-        //   this.setState({
-        //     alerts : data
-        //   })
-        // })
-        let response = new MockAlertHandler().getAlerts(startTime, endTime);
-        this.setState({
-          alerts : response
-        })
-      }, 3000);
-      return () => clearInterval();
-  }
-  
-    render() {
-        const {alerts} = this.state;
-        let dataSource = alerts;
-        // const dataSource = [
-        //     {
-        //       key: '1',
-        //       alert: 'asdfasdf',
-        //       time: 'March 09 2020',
-        //     },
-        //     {
-        //         key: '2',
-        //         alert: 'slksls',
-        //         time: 'March 09 2020',
-        //       },
-        //   ];
+        alerts: response
+      });
+    }, 3000);
+    return () => clearInterval();
+  };
 
-        const columns = [
-            {
-              title: 'Alert',
-              dataIndex: '_id',
-              key: '_id',
-            },
-            {
-              title: 'Time',
-              dataIndex: 'time',
-              key: 'time',
-            }
-          ];
-    
+  openDetailsModal = (record: any) => {
+    // new PersonHandler().getById(record._id).then(person => {
+    //   this.setState({
+    //     visible: true,
+    //     person: person
+    //   });
+    // });
+
+    let person = new MockPersonHandler().getById(record._id);
+    this.setState({
+      visible: true,
+      person: person
+    });
+  };
+
+  closeDetailsModal = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
+  render() {
+    const { alerts } = this.state;
+    let img = "data:image/jpeg;charset=utf-8;base64, " + this.state.person.img;
+    let dataSource = alerts;
+    // const dataSource = [
+    //     {
+    //       key: '1',
+    //       alert: 'asdfasdf',
+    //       time: 'March 09 2020',
+    //     },
+    //     {
+    //         key: '2',
+    //         alert: 'slksls',
+    //         time: 'March 09 2020',
+    //       },
+    //   ];
+
+    const columns = [
+      {
+        title: "Alert",
+        dataIndex: "_id",
+        key: "_id"
+      },
+      {
+        title: "Time",
+        dataIndex: "time",
+        key: "time"
+      }
+    ];
+
     return (
-        <>
-            <Title level={3}>Alerts</Title>
-            <Table dataSource={dataSource} columns={columns} />
-        </>
-    )
+      <>
+        <Title level={3}>Alerts</Title>
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: event => {
+                this.openDetailsModal(record);
+              } //click row, open alert details window
+            };
+          }}
+        />
+        <Modal
+          title="Alert Details"
+          visible={this.state.visible}
+          onOk={this.closeDetailsModal}
+          onCancel={this.closeDetailsModal}
+        >
+          <div style={{ display: "block" }}>
+            <Avatar
+              icon="user"
+              style={{ display: "inline-block", margin: 10 }}
+              src={img}
+              size={64}
+            />
+            <p style={{ display: "inline-block" }}>{this.state.person.name}</p>
+            <div
+              style={{ display: "inline-block", float: "right", margin: 10 }}
+            >
+              <ActionRequest id={this.state.person._id} />
+            </div>
+          </div>
+        </Modal>
+      </>
+    );
   }
 }
 
