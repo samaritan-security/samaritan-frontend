b935d92a355c599fa55e8acaaad043853a7806e8
Kate Brune <kbrune@iastate.edu>
2020-03-23 17:57:12 -0500

    made homepage dynamic for cameras in db

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import CameraInfo from "../../custom-components/cameraInfo/cameraInfo";
 import { Camera } from "../../api/api-types";
+import { CameraHandler } from "../../api/camera-handler";
 
-class HomePage extends React.Component {
+interface IHomePageProps {
+  handler: CameraHandler;
+}
+interface IHomePageState {
+  cameras: Camera[];
+}
+class HomePage extends React.Component<IHomePageProps, IHomePageState> {
+  state = {
+    cameras: []
+  };
+  componentDidMount = () => {
+    const { handler } = this.props;
+    handler.getCameras().then(data => {
+      this.setState({
+        cameras: data
+      });
+    });
+  };
   render() {
-    return (
-      <>
-        <CameraInfo
-          camera={new Camera({ _id: "0", nickname: "home", ip: "localhost" })}
-        />
-      </>
-    );
+    const { cameras } = this.state;
+    return cameras.map(camera => <CameraInfo camera={camera} />);
   }
 }
 
