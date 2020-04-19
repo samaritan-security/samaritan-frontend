d7a7ebde415ea2d8533bbc66d42de8c67815653b
Kate Brune <kbrune@iastate.edu>
2020-04-19 17:46:20 -0500

    Updates

---
+++ precache-manifest.2143d4a29b4f6c228016becf33dcee4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f0ec12c71b277f9738f17c5565977d",
+    "url": "/samaritan-frontend/index.html"
+  },
+  {
+    "revision": "3cb1ccc1d92cba70fc0d",
+    "url": "/samaritan-frontend/static/css/2.29c1ed91.chunk.css"
+  },
+  {
+    "revision": "9d5e50d52f45e085c831",
+    "url": "/samaritan-frontend/static/css/main.8bef6460.chunk.css"
+  },
+  {
+    "revision": "3cb1ccc1d92cba70fc0d",
+    "url": "/samaritan-frontend/static/js/2.b9a042b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/samaritan-frontend/static/js/2.b9a042b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5e50d52f45e085c831",
+    "url": "/samaritan-frontend/static/js/main.fb50908b.chunk.js"
+  },
+  {
+    "revision": "8a90e7df392967019f36",
+    "url": "/samaritan-frontend/static/js/runtime-main.322da46e.js"
+  }
+]);
